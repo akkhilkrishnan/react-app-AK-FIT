e85f82134237fe39b6f002f0082721fb1b118559
Krishnan <akkhil.krishnan@capgemini.com>
2024-10-10 05:33:45 +0530

    home changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,26 +2,70 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LoginPage from "../LoginPage";
 
-function Home (){
-    const [menuList, setMenuList] = useState(['Design Workout','Add New Member','View Member Details','Add Workouts'])
-    const handleClick = (menuName) => {
-        // setActiveTab(menuName)
-        }
+function Home() {
+  // const [menuList, setMenuList] = useState(['Design Workout','Add New Member','View Member Details','Add Workouts'])
+  const [membersData, setMembersData] = useState([]);
+  const currentDate = new Date();
+  const [currentSale, setCurrentSale] = useState(0);
+  const [targetSale, setTargetSale] = useState(450000);
+  let indianCurrency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'INR',
+    maximumFractionDigits:0
+});
+  useEffect(() => {
+    getmembersData();
+  }
+    , []);
 
-return (<><div className="home-container"> 
-<div class="container">
-  <h2 class="title">
-    <span class="title-word title-word-1">G </span>
-    <span class="title-word title-word-2">R </span>
-    <span class="title-word title-word-3">I </span>
-    <span class="title-word title-word-4">T </span>
-  </h2>
-</div>
-      {/* {menuList.map((menuItem, index) => {
-        return (<Link className='Home-widgets'key={index} to={menuItem.toLowerCase().replace(/ /g, '')} onClick={() => {
-          handleClick(menuItem);
-        }}>{menuItem.toUpperCase()}</Link>)
-      })} */}
+
+  const getmembersData = () => {
+    fetch("http://localhost:5000/data", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) =>
+        response.json()
+      )
+      .then((data) => {
+        console.log("data:::", data[1].amountpaid)
+        setMembersData(data);
+
+      });
+  };
+  useEffect(() => {
+    const totalAmount = membersData.reduce((acc, item) => {
+      const dojDate = new Date(item.doj);
+      if (dojDate.getMonth() === currentDate.getMonth() && dojDate.getFullYear() === currentDate.getFullYear()) {
+        return acc + Number(item.amountpaid);
+      }
+      return acc;
+    }, 0);
+
+    setCurrentSale(totalAmount);
+  }, [membersData, currentDate]);
+  return (<><div className="home-container">
+    <div style={{
+      display: 'flex', flexWrap:
+        "wrap", gap:"40px"
+    }}>
+      <div style={{display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+        <h1>CURRENT SALES</h1>
+        <div style={{ fontSize: '50px', color: 'Green' }}>  {indianCurrency.format(currentSale)}</div>
+        {/* <div style={{fontSize:'50px', color:'Green'}}>  {targetSale}</div> */}
+        <h1>TARGET</h1>
+        <div style={{ fontSize: '50px', color: 'Green' }}>  {indianCurrency.format(targetSale - currentSale)}</div>
+      </div>
+      <div>
+      <h1>CURRENT SALES</h1>
+        <div style={{ fontSize: '50px', color: 'Green' }}>  {indianCurrency.format(currentSale)}</div>
+        <h1>TARGET</h1>
+        <div style={{ fontSize: '50px', color: 'Green' }}>  {indianCurrency.format(targetSale - currentSale)}</div>
+      </div>
+    </div>
+
   </div></>)
 }
 export default Home
